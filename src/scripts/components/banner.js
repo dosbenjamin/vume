d8f318afc55630a037613cce6fa31be3330a3b8d
Benjamin Dos Santos <dosbenjamin@gmail.com>
2019-11-10 19:28:39 +0100

    :rocket: Add banner animation

--- src/scripts/components/banner.js
+++ src/scripts/components/banner.js
@@ -1,17 +1,41 @@
+const howManyWords = $el => {
+  const windowWidth = document.body.offsetWidth
+  const elWidth = $el.firstElementChild.offsetWidth
+  const numberOfEl = Math.round(windowWidth / elWidth) + 3
+  return { elWidth, numberOfEl }
+}
+
+const createStyle = width => {
+  return `
+    @keyframes banner-${width} {
+      from {
+        margin-left: -${width}px;
+      }
+    }
+  `
+}
+
 export default {
-  generate ($words) {
+  generate () {
+    const $words = document.querySelectorAll('.banner__item')
+    const $style = document.querySelector('style')
+    let style = ''
+
     $words.forEach($el => {
-      const windowWidth = document.body.offsetWidth
-      const elWidth = $el.firstElementChild.offsetWidth
-      const numberOfEl = Math.round(windowWidth / elWidth) + 1
       const bannerContent = $el.firstElementChild.textContent
-      let template = ''
+      const { elWidth, numberOfEl } = howManyWords($el)
 
+      let span = ''
       for (let i = 0; i < numberOfEl; i++) {
-        template += `<span>${bannerContent}</span>`
-      };
+        span += `<span class="banner__el">${bannerContent}</span>`
+      }
+      style += createStyle(elWidth)
 
-      $el.innerHTML = template
+      $el.style.width = elWidth * numberOfEl + 'px'
+      $el.innerHTML = span
+
+      $el.firstChild.setAttribute('style', `animation-name: banner-${elWidth}`)
     })
+    $style.innerHTML = style
   }
 }
